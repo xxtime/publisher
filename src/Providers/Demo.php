28ec86e3117d830a0291a549b77a77ff52c3da34
Joe <joe@xxtime.com>
2017-06-16 11:55:55 +0800

    更新Demo

--- src/Providers/Demo.php
+++ src/Providers/Demo.php
@@ -41,19 +41,20 @@ class Demo extends ProviderAbstract
     /**
      * @param array $parameter
      *    $parameter = [
-     *        'transaction'  => '',
-     *        'amount'       => '',
-     *        'currency'     => '',
-     *        'product_id'   => '',
-     *        'product_name' => '',
+     *        'transaction'  => '', // 平台订单ID
+     *        'amount'       => '', // 金额
+     *        'currency'     => '', // 货币种类
+     *        'product_id'   => '', // 产品ID
+     *        'product_name' => '', // 产品名称
+     *        'raw'          => '', // 用户登录发行渠道返回的原始数据， verifyToken 方法返回的 original字段
      *    ];
      * @return array
      */
     public function tradeBuild($parameter = [])
     {
         return [
-            'reference' => '',   // 发行商订单号
-            'raw'       => ''    // 原始返回数组
+            'reference' => '',      // 发行商订单号
+            'raw'       => []       // 发行渠道返回的原始信息, 也可添加额外参数
         ];
     }
 
