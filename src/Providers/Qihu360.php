bc1aae0b5293d40507145886f8ff29bbd74ddf99
lihe <1395992734@qq.com>
2017-06-19 10:41:08 +0800

    更新qihu360

--- src/Providers/Qihu360.php
+++ src/Providers/Qihu360.php
@@ -45,23 +45,20 @@ class Qihu360 extends ProviderAbstract
             throw  new  DefaultException('error order');
         }
 
-        $param['product_sn'] = $_REQUEST['product_id'];                //应用自定义的商品id
+        $param['product_id'] = $_REQUEST['product_id'];                //应用自定义的商品id
         $param['amount'] = $_REQUEST['amount'];                        // 总价，单位(分)
-        $param['order_id'] = $_REQUEST['app_order_id'];                // 订单id
-        $param['user_id'] = $_REQUEST['app_uid'];                      // 应用分配给用户的id
-        $param['role_id'] = $_REQUEST['app_ext1'];                     // 角色id
-        $param['server_id'] = $_REQUEST['app_ext2'];                   // 服务器id
+        $param['app_order_id'] = $_REQUEST['app_order_id'];                // 订单id
+        $param['app_uid'] = $_REQUEST['app_uid'];                      // 应用分配给用户的id
 
         // 其他参数
-        $param['gateway_status'] = $gateway_flag;                       // 如果支付返回成功,返回success 应用需要确认是 success 才给用户加钱
-        $param['partner_user_id'] = $_REQUEST['user_id'];               // 360账号id
-        $param['partner_order_id'] = $_REQUEST['order_id'];             // 360订单号
+        $param['gateway_flag'] = $gateway_flag;                       // 如果支付返回成功,返回success 应用需要确认是 success 才给用户加钱
+        $param['user_id'] = $_REQUEST['user_id'];                     // 360账号id
+        $param['order_id'] = $_REQUEST['order_id'];                   // 360订单号
 
         $param['app_key'] = $_REQUEST['app_key'];                       // 应用app_key
         $param['sign_type'] = $_REQUEST['sign_type'];                   // 定值md5
         $sign_return = $_REQUEST['sign_return'];                        // 应用回传给订单核实接口 的参数 不加入签名校验计算
         $sign = $_REQUEST['sign'];                                      // 签名
-
         //检查签名
         $this->check_sign($param, $sign);
 
@@ -83,11 +80,10 @@ class Qihu360 extends ProviderAbstract
                 unset($data[$k]);
             }
         }
-        $data = ksort($data);
-        $sign_str = implode('#', $data);
+        ksort($data);
+        $sign_str = implode("#", $data);
         $secretkey = $this->option['secret_key'];
         $sign_str = $sign_str . '#' . $secretkey;
-
         if (strtolower($sign) != strtolower(md5($sign_str))) {
             throw new DefaultException('sign error');
         }
