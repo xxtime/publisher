6b1a154e9b894eadf94cc0e7da95f69d3322f0d2
unknown <346257592@qq.com>
2017-08-08 18:04:35 +0800

    新增悦游、誉碧接口

---
+++ src/Providers/Yubi.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lkl
+ * Date: 2017/8/8
+ * Time: 15:17
+ */
+
+namespace Xt\Publisher\Providers;
+
+use Xt\Publisher\DefaultException;
+
+class Yubi extends ProviderAbstract
+{
+    public function verifyToken($token = '', $option = [])
+    {
+        $url = 'http://www.i7game.com/sdk.php/LoginNotify/login_verify';
+
+        $param = [
+            'user_id' => $option['user_id'],
+            'token'   => $token,
+        ];
+
+        $response = $this->http_curl_post($url, $param);
+
+        $result = json_decode($response, true);
+
+        //如果遇到错误 则抛出错误
+        if ($result['status'] != 1) {
+            throw new DefaultException($response);
+        }
+
+        return [
+            'uid'      => $result['user_id'],
+            'username' => $result['user_account'],
+            'original' => $result
+        ];
+    }
+
+    private function http_curl_post($url, $data, $extend = array())
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_NOBODY, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        $curl_result = curl_exec($ch);
+        curl_close($ch);
+
+        return $curl_result;
+    }
+
+    public function notify()
+    {
+        $req = $_REQUEST;
+        $data = array(
+            'out_trade_no' => $req['out_trade_no'],
+            'price'        => $req['price'],
+            'pay_status'   => $req['pay_status'],
+            'extend'       => $req['extend'],
+        );
+
+        $sign = $req['sign'];
+
+        $str1 = '';
+        foreach ($data as $k => $v) {
+            $str1 .= "$v";
+        }
+        $mysign = md5($str1 . $this->app_key);
+
+        if ($sign != $mysign) {
+            throw new DefaultException('sign error');
+        }
+
+        // 平台参数
+        $param['amount'] = round($data['price'] / 100, 2);                              // 总价.单位: 分
+        $param['transaction'] = $data['out_trade_no'];                              // 订单id
+        $param['currency'] = 'CNY';                                                         // 货币类型
+        $param['reference'] = $data['out_trade_no'];                           // 第三方订单ID
+        $param['userId'] = '';                                   // 第三方账号ID
+
+        return $param;
+    }
+
+    public function success()
+    {
+        exit('success');
+    }
+}
---
+++ src/Providers/Yueyou.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lkl
+ * Date: 2017/8/8
+ * Time: 17:27
+ */
+namespace Xt\Publisher\Providers;
+
+use Xt\Publisher\DefaultException;
+
+class yueyou extends ProviderAbstract
+{
+    public function verifyToken($token = '', $option = [])
+    {
+        $url = 'http://rhsdk.yueeyou.com/api/cp/checkToken';
+
+        $time = time();
+        $data = [
+            'AppID'       => $this->app_id,
+            'timestamp' => $time,
+            'token'   => $token
+        ];
+
+        $str1 = '';
+        foreach ($data as $k => $v) {
+            $str1 .= "$k=$v&";
+        }
+        $str1 = trim($str1, '&');
+        $data['sign'] = md5($str1 . $this->app_key);
+
+        $param = http_build_query($data);
+
+        $url = $url . $param;
+        $response = file_get_contents($url);
+        $result = json_decode($response, true);
+
+        //如果有异常 抛出异常
+        if ($result['code'] != 1) {
+            throw new DefaultException($response);
+        }
+
+        return array('uid' => $result['userId'], 'username' => '', 'original' => (array)$result);
+    }
+
+    public function notify()
+    {
+        $req = $_REQUEST;
+
+        $data = array(
+            'result' => $req['result'],
+            'amount'        => $req['amount'],
+            'channelId'   => $req['channelId'],
+            'appId'       => $req['appId'],
+            'appId'       => $req['appId'],
+            'orderId'       => $req['orderId'],
+            'cpOrderId'       => $req['cpOrderId'],
+            'userId'       => $req['userId'],
+            'timestamp'       => $req['timestamp'],
+            'ext'       => $req['ext'],
+        );
+        ksort($data);
+        $sign = $req['sign'];
+
+        $str1 = '';
+        foreach ($data as $k => $v) {
+            $str1 .= "$k=$v&";
+        }
+        $str1 = trim($str1, '&');
+        $mysign = md5($str1 . $this->app_key);
+
+        if ($sign != $mysign) {
+            throw new DefaultException('sign error');
+        }
+
+        // 平台参数
+        $param['amount'] = round($data['amount'] / 100, 2);                              // 总价.单位: 分
+        $param['transaction'] = $data['cpOrderId'];                              // 订单id
+        $param['currency'] = 'CNY';                                                         // 货币类型
+        $param['reference'] = $data['orderId'];                           // 第三方订单ID
+        $param['userId'] = '';                                   // 第三方账号ID
+
+        return $param;
+    }
+
+    public function success()
+    {
+        exit('OK');
+    }
+}
