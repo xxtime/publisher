e46ade94d51f921af4156660b62d87ed1d7cb831
lihe <1395992734@qq.com>
2019-02-11 10:00:29 +0800

    新增QuickSDK

---
+++ src/Providers/Quick.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lihe
+ * Date: 2019/2/11
+ * Time: 9:49 AM
+ */
+namespace Xt\Publisher\Providers;
+
+class Quick extends ProviderAbstract {
+
+    // 登录验证
+    public function verifyToken($token = '', $option = [])
+    {
+        // TODO: Implement verifyToken() method.
+    }
+
+    // 充值验证
+    public function notify()
+    {
+        // TODO: Implement notify() method.
+    }
+}
